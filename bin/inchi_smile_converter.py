7df9aaff9c6adbb60429a95e0b441534b066de7e
Mingxun Wang <mwang87@gmail.com>
2025-06-16 11:17:10 -0700

    fixing urls

--- bin/inchi_smile_converter.py
+++ bin/inchi_smile_converter.py
@@ -6,7 +6,7 @@ import urllib.parse
 
 # this is used to query inchi, smiles using Ming's web api
 def inchi2smiles (inchi):
-    url = "https://gnps-structure.ucsd.edu/smiles?inchi={}".format(urllib.parse.quote(inchi))
+    url = "https://structure.gnps2.org/smiles?inchi={}".format(urllib.parse.quote(inchi))
     if response.status_code !=200 :
         #print(response.text)
         print(url)
@@ -15,7 +15,7 @@ def inchi2smiles (inchi):
     return response.text
 
 def smiles2inchi (smiles):
-    url = "https://gnps-structure.ucsd.edu/inchi?smiles={}".format(urllib.parse.quote(smiles))
+    url = "https://structure.gnps2.org/inchi?smiles={}".format(urllib.parse.quote(smiles))
     response = requests.get(url)
     if response.status_code !=200 :
         #print(response.text)
